7564f209ebfaa262f1b588e10af9d45111bdd981
adammcc <ajm845@gmail.com>
2014-12-28 18:07:44 -0500

    Format change for seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,18 +34,20 @@ subjects.each do |subject_name|
 end
 
 30.times do
-	user = User.create(demo: true,
-                     first_name: Faker::Name.first_name,
-                     last_name: Faker::Name.last_name,
-                     email: Faker::Internet.email,
-                     password: "asdfasdf",
-                     password_confirmation: "asdfasdf",
-                     pro_summary: Faker::Lorem.paragraph(sentence_count = 3, supplemental = false, random_sentences_to_add = 3),
-                     skill: "Bowling",
-                     headline: Faker::Lorem.sentence(word_count = 5, supplemental = false, random_words_to_add = 6),
-                     image_url: Faker::Avatar.image,
-                     roles: [Role.all.sample],
-                     subjects: [Subject.all.sample])
+	user = User.create(
+    demo: true,
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    password: "asdfasdf",
+    password_confirmation: "asdfasdf",
+    pro_summary: Faker::Lorem.paragraph(sentence_count = 3, supplemental = false, random_sentences_to_add = 3),
+    skill: "Bowling",
+    headline: Faker::Lorem.sentence(word_count = 5, supplemental = false, random_words_to_add = 6),
+    image_url: Faker::Avatar.image,
+    roles: [Role.all.sample],
+    subjects: [Subject.all.sample]
+  )
 
 	4.times do
 		user.skills.create(:name => skills.sample)
