acd41dfc38b22a9426bba112ae1ffd57a7a0dbff
adammcc <ajm845@gmail.com>
2015-03-08 23:28:14 -0400

    Fix bug in users index
    - Bug occurred when user did not have subjects or 
    roles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,26 +43,39 @@ class UsersController < ApplicationController
     @roles = Role.all
     @subjects = Subject.all
 
-    college_user_ids = []
-    @user.colleges.each do |college|
-      college_user_ids << college.user_ids
+    if @user.has_colleges?
+      college_user_ids = []
+      @user.colleges.each do |college|
+        college_user_ids << college.user_ids
+      end
+      college_user_ids.flatten!
+      college_user_suggestions = User.in(id: college_user_ids).ne(id: @user.id)
+    else
+      college_user_suggestions = []
     end
-    college_user_ids.flatten!
-    college_user_suggestions = User.in(id: college_user_ids).ne(id: @user.id)
 
-    subject_user_ids = []
-    @user.subjects.each do |subject|
-      subject_user_ids << subject.user_ids
+    if @user.has_subjects?
+      subject_user_ids = []
+      @user.subjects.each do |subject|
+        subject_user_ids << subject.user_ids
+      end
+      subject_user_ids.flatten!
+      subject_user_suggestions = User.in(id: subject_user_ids).ne(id: @user.id)
+    else
+      subject_user_suggestions = []
     end
-    subject_user_ids.flatten!
-    subject_user_suggestions = User.in(id: subject_user_ids).ne(id: @user.id)
 
     if @user.is_admin?
       admin_role = Role.where(name: 'Administrator').first
       role_user_suggestions = admin_role.users.ne(id: @user.id)
+      role_user_suggestions = [] if role_user_suggestions.blank?
+    else
+      role_user_suggestions = []
     end
 
     @suggestions = (college_user_suggestions + subject_user_suggestions + role_user_suggestions).shuffle.first(3)
+
+    @suggestions = User.all.ne(id: @user.id).shuffle.first(3) if @suggestions.blank?
   end
 
   def new
