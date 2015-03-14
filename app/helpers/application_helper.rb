2fbd2653c917f9e0e49a092409251751d55b8f74
adammcc <ajm845@gmail.com>
2015-03-14 17:01:56 -0400

    Adds user_image helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+  def user_image(user, type = '', height, width)
+    image_tag user.photo_url(type), onerror: "imgError(this);", height: height, width: width
+  end
+
 end
