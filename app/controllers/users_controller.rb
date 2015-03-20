d060a298263dc4eb0a1098c574436fc15eab17c2
adammcc <ajm845@gmail.com>
2015-03-19 23:32:48 -0400

    Bug Fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,40 +42,7 @@ class UsersController < ApplicationController
     @experiences = @user.experiences
     @roles = Role.all
     @subjects = Subject.all
-
-    if @user.has_colleges?
-      college_user_ids = []
-      @user.colleges.each do |college|
-        college_user_ids << college.user_ids
-      end
-      college_user_ids.flatten!
-      college_user_suggestions = User.in(id: college_user_ids).ne(id: @user.id, as_org: true)
-    else
-      college_user_suggestions = []
-    end
-
-    if @user.has_subjects?
-      subject_user_ids = []
-      @user.subjects.each do |subject|
-        subject_user_ids << subject.user_ids
-      end
-      subject_user_ids.flatten!
-      subject_user_suggestions = User.in(id: subject_user_ids).ne(id: @user.id, as_org: true)
-    else
-      subject_user_suggestions = []
-    end
-
-    if @user.is_admin?
-      admin_role = Role.where(name: 'Administrator').first
-      role_user_suggestions = admin_role.users.ne(id: @user.id, as_org: true)
-      role_user_suggestions = [] if role_user_suggestions.blank?
-    else
-      role_user_suggestions = []
-    end
-
-    @suggestions = (college_user_suggestions + subject_user_suggestions + role_user_suggestions).shuffle.first(3)
-
-    @suggestions = User.all.ne(id: @user.id, as_org: true).shuffle.first(3) if @suggestions.blank?
+    @suggestions = get_suggestions(@user)
   end
 
   def new
@@ -146,6 +113,7 @@ class UsersController < ApplicationController
   def approve_friend
     @user = current_user
     @friend = User.find(params[:id])
+    @suggestions = get_suggestions(@user)
 
     current_user.approve @friend
     current_user.save
@@ -154,6 +122,7 @@ class UsersController < ApplicationController
   def remove_deny_friend
     @user = current_user
     @friend = User.find(params[:id])
+    @suggestions = get_suggestions(@user)
 
     current_user.remove_friendship @friend
     current_user.save
@@ -193,4 +162,40 @@ class UsersController < ApplicationController
   end
 
   private
+
+  def get_suggestions(user)
+    if user.has_colleges?
+      college_user_ids = []
+      user.colleges.each do |college|
+        college_user_ids << college.user_ids
+      end
+      college_user_ids.flatten!
+      college_user_suggestions = User.in(id: college_user_ids).ne(id: user.id, as_org: true)
+    else
+      college_user_suggestions = []
+    end
+
+    if user.has_subjects?
+      subject_user_ids = []
+      user.subjects.each do |subject|
+        subject_user_ids << subject.user_ids
+      end
+      subject_user_ids.flatten!
+      subject_user_suggestions = User.in(id: subject_user_ids).ne(id: user.id, as_org: true)
+    else
+      subject_user_suggestions = []
+    end
+
+    if user.is_admin?
+      admin_role = Role.where(name: 'Administrator').first
+      role_user_suggestions = admin_role.users.ne(id: user.id, as_org: true)
+      role_user_suggestions = [] if role_user_suggestions.blank?
+    else
+      role_user_suggestions = []
+    end
+
+    @suggestions = (college_user_suggestions + subject_user_suggestions + role_user_suggestions).shuffle.first(3)
+    @suggestions = User.all.ne(id: user.id, as_org: true).shuffle.first(3) if @suggestions.blank?
+  end
+
 end
--- app/views/orgs/approve_friend.js.coffee
+++ app/views/orgs/approve_friend.js.coffee
@@ -4,7 +4,7 @@ $(".js_users--connection-<%= @friend.id %>").html("<%= j new_html %>")
 <% new_html = render(partial: 'users/profile_actions', locals: { user: @friend }) %>
 $(".js-profile-actions").html("<%= j new_html %>")
 
-<% new_html = render(partial: 'users/connections', locals: { user: @user }) %>
+<% new_html = render(partial: 'users/connections', locals: { user: @user, suggestions: @suggestions }) %>
 $(".js-profile-section--connections").html("<%= j new_html %>")
 
 $('#ep-invited').hide()
